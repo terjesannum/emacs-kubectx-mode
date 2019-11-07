58e46a5fe089df0e8012d08b8f56fb7d753a1a49
Terje Sannum <terje.sannum@nav.no>
2019-11-07 07:38:29 +0100

    renamed to kubectx-mode

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# emacs-kubectx-mode-line
+# emacs-kubectx-mode
 
 Display current kubectl context and namespace in Emacs mode line.
 
 ```lisp
-(add-to-list 'load-path "~/.emacs.d/emacs-kubectx-mode-line")
-(require 'kubectx)
+(add-to-list 'load-path "~/.emacs.d/emacs-kubectx-mode")
+(require 'kubectx-mode)
 ```
 
 Enable it with `M-x kubectx-mode` or `(kubectx-mode 1)`
 
 The mode line will be updated every 10 seconds or what is defined in
-`kubectx-update-interval`. 
+`kubectx-mode-mode-line-update-interval`. 
 
 You can also trigger the update with `emacsclient` as a `PROMPT_COMMAND`:
 
 ```bash
-export PROMPT_COMMAND="emacsclient -e '(kubectx-update)' &>/dev/null"
+export PROMPT_COMMAND="emacsclient -e '(kubectx-mode-mode-line-update)' &>/dev/null"
 ```
---
+++ kubectx-mode.el
@@ -0,0 +1,72 @@
+;;; kubectx-mode.el --- Global minor-mode to change kubectl context and namespace and display in Emacs mode line
+
+;; Copyright (C) 2018-2019 Terje Sannum
+
+;; Author: Terje Sannum <terje@offpiste.org>
+;; Created: 19 Oct 2018
+;; Keywords: mode-line kubernetes
+;; Homepage: https://github.com/terjesannum/emacs-kubectx-mode
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See https://github.com/terjesannum/emacs-kubectx-mode/blob/master/README.md
+
+;;; Code:
+(defvar kubectx-mode-kubectl-command "kubectl" "Kubectl binary")
+(defvar kubectx-mode-timer nil)
+(defvar kubectx-mode-mode-line-string "")
+(defvar kubectx-mode-mode-line-update-interval 10 "Number of seconds between background mode-line updates")
+(defvar kubectx-mode-mode-line-string-format " [kube:%C %N]" "String to display in mode-line (%C = context, %N = namespace)")
+
+(defun kubectx-mode-run-kubectl (&rest args)
+  "Run kubectl command"
+  (with-temp-buffer
+    (let ((default-directory "~"))
+      (if (and (executable-find kubectx-mode-kubectl-command)
+               (= (apply 'call-process kubectx-mode-kubectl-command nil t nil args) 0))
+          (replace-regexp-in-string "\n\\'" "" (buffer-string))
+        "n/a"))))
+
+(defun kubectx-mode-kubectx-string (context namespace)
+  "Create kubectx string to display"
+  (replace-regexp-in-string "%C" context (replace-regexp-in-string "%N" namespace kubectx-mode-mode-line-string-format t) t))
+
+(defun kubectx-mode-mode-line-update ()
+  "Update kubectx mode-line string with current context and namespace"
+  (interactive)
+  (let ((ctx (kubectx-mode-run-kubectl "config" "current-context"))
+        (ns (kubectx-mode-run-kubectl "config" "view" "--minify" "--output" "jsonpath={..namespace}")))
+    (setq kubectx-mode-mode-line-string (kubectx-mode-kubectx-string ctx ns))
+    (force-mode-line-update t)))
+
+(define-minor-mode kubectx-mode
+  "Add kubectx and namespace info to the mode line"
+  :global t
+  (when (not global-mode-string) (setq global-mode-string '("")))
+  (when kubectx-mode-timer (cancel-timer kubectx-mode-timer))
+  (if (not kubectx-mode)
+      (setq global-mode-string
+            (delq 'kubectx-mode-mode-line-string global-mode-string))
+    (add-to-list 'global-mode-string 'kubectx-mode-mode-line-string t)
+    (when (> kubectx-mode-mode-line-update-interval 0)
+      (setq kubectx-mode-timer
+            (run-at-time nil kubectx-mode-mode-line-update-interval
+                         'kubectx-mode-mode-line-update)))
+    (kubectx-mode-mode-line-update)))
+
+(provide 'kubectx-mode)
+
+;;; kubectx-mode.el ends here
