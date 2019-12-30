51707cab3a4b4121bc02305aa57984c113132758
Terje Sannum <terje.sannum@nav.no>
2019-12-30 14:02:58 +0100

    Package-Version -> Version

--- kubectx-mode.el
+++ kubectx-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Terje Sannum <terje@offpiste.org>
 ;; Keywords: tools kubernetes
-;; Package-Version: 1.0
+;; Version: 1.0
 ;; Package-Requires: ((emacs "24"))
 ;; URL: https://github.com/terjesannum/emacs-kubectx-mode
 
